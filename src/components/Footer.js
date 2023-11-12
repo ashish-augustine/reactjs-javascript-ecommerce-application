22d7516911e1e4ad754ca86f245fcd11c9023e5b
Ashish Augustine <ashish.augustine123@gmail.com>
2023-11-12 13:43:33 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span> ComfySloth</span>
+        <span> Ashish Augustine</span>
       </h5>
       <h5>All rights reserved</h5>
     </Wrapper>
