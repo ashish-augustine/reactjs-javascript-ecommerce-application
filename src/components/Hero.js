1b7bc87e8ea829da106278ac42592ae971dd7e83
Ashish Augustine <ashish.augustine123@gmail.com>
2023-11-23 15:32:20 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,8 +13,8 @@ const Hero = () => {
           furniture online
         </h1>
         <p>
-          This is an e-commerce application built using React JS, listing a range of furniture products for home & office. The
-          products can be filtered on the basis of their Category, Company, Colour, Price & Shipping Charges. The products can be
+          This is an e-commerce application listing a range of furniture products for home & office. You can design, customise & buy your furniture
+    from our online store. A one stop solution for designing & decorating your home! The products can be filtered on the basis of their Category, Company, Colour, Price & Shipping Charges. The products can be
           added to the cart and then proceeded to the checkout. The checkout is integrated with Stripe Payment.
         </p>
         <Link to="/products" className="btn hero-btn">
