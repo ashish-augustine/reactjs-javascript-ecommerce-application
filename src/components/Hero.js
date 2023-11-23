d50f0e48f221187e1053bf70272214ac812e7183
Ashish Augustine <ashish.augustine123@gmail.com>
2023-11-23 20:49:01 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,9 +13,9 @@ const Hero = () => {
           furniture online
         </h1>
         <p>
-          This is an e-commerce application listing a range of furniture products for home & office. You can design, customise & buy your furniture
-    from our online store. A one stop solution for designing & decorating your home! The products can be filtered on the basis of their Category, Company, Colour, Price & Shipping Charges. The products can be
-          added to the cart and then proceeded to the checkout. The checkout is integrated with Stripe Payment.
+          You can customise & buy furniture
+    from our online store.  A one stop solution for designing & decorating your home! This is an e-commerce application listing a range of furniture products for home & office. The products can be filtered on the basis of their Category, Company, Colour, Price & Shipping Charges. The products can be
+          added to the cart and then proceeded to the checkout. The checkout allows you to pay with your Debit / Credit cards.
         </p>
         <Link to="/products" className="btn hero-btn">
           shop now
